0e682ed1e21eb6faf88a57e78db10b8bf7dd8ecc
Jaehwa Noh <shwoghk14@gmail.com>
2026-03-15 22:21:51 +0900

    Apply all azul architecture.

---
+++ resolve_toolchain_plugin/src/main/kotlin/starlightlab/jaehwa/resolve_toolchain_plugin/urls/AllUrls.kt
@@ -0,0 +1,7 @@
+package starlightlab.jaehwa.resolve_toolchain_plugin.urls
+
+import starlightlab.jaehwa.resolve_toolchain_plugin.urls.azul.azulUrlMap
+
+internal val allUrlsMap = buildMap {
+    putAll(azulUrlMap)
+}
