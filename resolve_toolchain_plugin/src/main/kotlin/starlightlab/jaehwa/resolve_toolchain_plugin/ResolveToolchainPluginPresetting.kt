b6c49377547d000d0464f44be1ff6ce5fe2634d2
Jaehwa Noh <shwoghk14@gmail.com>
2026-03-15 22:46:42 +0900

    Set publish options.

--- resolve_toolchain_plugin/src/main/kotlin/starlightlab/jaehwa/resolve_toolchain_plugin/ResolveToolchainPluginPresetting.kt
+++ resolve_toolchain_plugin/src/main/kotlin/starlightlab/jaehwa/resolve_toolchain_plugin/ResolveToolchainPluginPresetting.kt
@@ -1,12 +1,18 @@
 package starlightlab.jaehwa.resolve_toolchain_plugin
 
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.kotlin.dsl.jvm
+import org.gradle.util.GradleVersion
 
 abstract class ResolveToolchainPluginPresetting : Plugin<Settings> {
 
     override fun apply(settings: Settings) {
+        val minGradle = GradleVersion.version("7.6")
+        if (GradleVersion.current() < minGradle) {
+            throw GradleException("${minGradle.version} and above requires to use toolchain resolver plugin")
+        }
 
         with(settings) {
             plugins.apply("io.github.jaehwa-noh.resolve-toolchain-plugin")
