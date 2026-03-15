cd61125bc92b639e11eabb904636b02e1bf275f5
Jaehwa Noh <shwoghk14@gmail.com>
2026-03-15 22:13:03 +0900

    Update OperatingSystemExtension.kt

--- resolve_toolchain_plugin/src/main/kotlin/starlightlab/jaehwa/resolve_toolchain_plugin/extensions/OperatingSystemExtension.kt
+++ resolve_toolchain_plugin/src/main/kotlin/starlightlab/jaehwa/resolve_toolchain_plugin/extensions/OperatingSystemExtension.kt
@@ -3,7 +3,7 @@ package starlightlab.jaehwa.resolve_toolchain_plugin.extensions
 import org.gradle.platform.OperatingSystem
 
 internal const val LINUX = "linux"
-internal const val WINDOW = "window"
+internal const val WINDOWS = "windows"
 internal const val MAC = "mac"
 internal const val SOLARIS = "solaris"
 
@@ -13,7 +13,7 @@ internal const val SOLARIS = "solaris"
 internal fun OperatingSystem.getAzulSupportOs() =
     when (this) {
         OperatingSystem.LINUX, OperatingSystem.UNIX, OperatingSystem.FREE_BSD -> LINUX
-        OperatingSystem.WINDOWS -> WINDOW
+        OperatingSystem.WINDOWS -> WINDOWS
         OperatingSystem.MAC_OS -> MAC
         OperatingSystem.SOLARIS -> SOLARIS
     }
